a29f0f0ddcaa8513fcde59eef84a62d073dd5f89
PompeyXu <hb_xupeng@163.com>
2019-04-06 17:03:03 +0800

    初始化

---
+++ src/main/java/com/pompey/upms/PompeyUpmsApplication.java
@@ -0,0 +1,13 @@
+package com.pompey.upms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PompeyUpmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PompeyUpmsApplication.class, args);
+    }
+
+}
